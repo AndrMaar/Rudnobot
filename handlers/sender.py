ccad737e6044457f15ad7e121b253b50fb8add05
Andry <andrymar2008@mail.ru>
2025-03-27 12:38:38 +0700

    починил рассылку

--- handlers/sender.py
+++ handlers/sender.py
@@ -5,7 +5,7 @@ from db.database import find_id
 async def send(bot):
     while True:
         now = datetime.now(timezone.utc)  # Указали часовой пояс
-        next_hour = (now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1))  # Исправили на hours=1
+        next_hour = (now.replace(minute=0, second=0, microsecond=0) + timedelta(minutes=1))
         wait_time = (next_hour - now).total_seconds()
         await asyncio.sleep(wait_time)
         await send_bulk_message(bot)
@@ -15,7 +15,7 @@ async def send_bulk_message(bot):
     for user_id in user_ids:
         try:
             await bot.send_message(user_id, "Отправьте свой статус")
-            await asyncio.sleep(0.1)  # Добавили антифлуд
+            await asyncio.sleep(3600)
         except Exception as e:
             print(f"Error sending to {user_id}: {e}")  #
 
